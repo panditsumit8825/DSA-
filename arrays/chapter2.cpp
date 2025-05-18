859cf2573f2d4b748b1031cd91a9c2fa3befd816
sumit kumar <panditsumit8825@gmail.com>
2025-05-18 19:40:07 +0530

    first changes

--- arrays/chapter2.cpp
+++ arrays/chapter2.cpp
@@ -94,32 +94,135 @@ using namespace std;
 
 
     // intersection in array
-    int main(){
-        int arr[]={1,2,3,4,5,6,7,8};
-        int sizea=8;
-        int brr[]={1,2,3,4,9};
-        int sizeb=5;
-        vector<int> ans;
-        // outer loop on arr vector
-        for (int i = 0; i < sizea; i++)
-        {
-            int element = arr[i];
-        //  for array b
-        for (int j = 0;  j< sizeb; j++)
-        {
-            if (element == brr[j])
-            {
-                ans.push_back(element);
-            }
-        }
-    }
-    for (auto value:ans)
-    {
-        cout<<value<<" ";
-    }
-    cout<<endl;
+    // int main(){
+    //     int arr[]={1,2,3,3,4,5,6,7,8};
+    //     int sizea=8;
+    //     int brr[]={1,2,3,4,3,9};
+    //     int sizeb=5;
+    //     vector<int> ans;
+    //     // outer loop on arr vector
+    //     for (int i = 0; i < sizea; i++)
+    //     {
+    //         int element = arr[i];
+    //     //  for array b
+    //     for (int j = 0;  j< sizeb; j++)
+    //     {
+    //         if (element == brr[j])
+    //         {
+    //             // mark
+    //             brr[j]=-1;
+    //             ans.push_back(element);
+    //         }
+    //     }
+    // }
+    // for (auto value:ans)
+    // {
+    //     cout<<value<<" ";
+    // }
+    // cout<<endl;
     
         
+    // Pair some
+
+    // int main(){
+    //     vector<int> arr{10,20,30,40,50,60,70};
+    //     int sum=100;
+    //     // print all pairs
+    //     // outer loop traverse for each element
+    //     for (int i = 0; i < arr.size(); i++)
+    //     {
+    //         int element = arr[i];
+    //         // for every elements traverse for apne aage wale elements
+    //         for (int j = i+1; j < arr.size(); j++)
+    //         {
+    //             // cout<<"("<<element<<","<<arr[j]<<")"<<endl;
+    //             if (element+arr[j]==sum)
+    //             {
+    //                 cout<<"Pair found "<<element<<","<<arr[j]<<endl;
+    //             }
+                
+    //         }
+            
+    //     }
+
+    // three pair sum
+    // int main(){
+    //     vector<int> arr={10,20,30,40,60,80,70};
+    //     int sum=100;
+    //     for (int i = 0; i < arr.size(); i++)
+    //     {
+    //         int element1=arr[i];
+    //         for (int j = i+1; j < arr.size(); j++)
+    //         {
+    //             int element2=arr[j];
+    //             for (int k = i+2; k < arr.size(); k++)
+    //             {
+    //                 int element3=arr[k];
+    //                 if (element1+element2+element3==sum)
+    //                 {
+    //                     cout<<element1<<","<<element2<<","<<element3<<" "<<endl;
+    //                 }
+                    
+    //             }
+                
+    //         }
+            
+    //     }
+
+    // four sum
+    //     int main(){
+    //     vector<int> arr={10,20,30,40,60,80,70};
+    //     int sum=150;
+    //     for (int i = 0; i < arr.size(); i++)
+    //     {
+    //         int element1=arr[i];
+    //         for (int j = i+1; j < arr.size(); j++)
+    //         {
+    //             int element2=arr[j];
+    //             for (int k = i+2; k < arr.size(); k++)
+    //             {
+    //                 int element3=arr[k];
+    //                 for (int p = k+1; p < arr.size(); p++)
+    //                 {
+    //                     int element4=arr[p];
+    //                     if (element1+element2+element3+element4==sum)
+    //                 {
+    //                     cout<<element1<<","<<element2<<","<<element3<<","<<element4<<" "<<endl;
+    //                 }
+    //                 }
+                    
+    //             }
+                
+    //         }
+            
+    //     }
+        
+        
+    // sort 0's and 1's
+    int main(){
+        vector<int> arr={0,1,0,0,1,1,1,0,1,0,1};
+        int start=0;
+        int end=arr.size()-1;
+        int i=0;
+        while (i!=end)
+        {
+            if (arr[i]==0)
+            {
+                swap(arr[start],arr[i]);
+                i++;
+                start++;
+            }
+            else{
+                swap(arr[i],arr[end]);
+                end--;
+            }
+        }
+        for(auto value:arr){
+            cout<<value<<" ";
+        }
+        cout<<endl;
+        
+
     return 0;
 
 
