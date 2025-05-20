1c5a6c32d893aaf67dba3ac238e13a9bb3ec97ed
sumit kumar <panditsumit8825@gmail.com>
2025-05-20 10:49:30 +0530

    first changes

--- arrays/chapter3.cpp
+++ arrays/chapter3.cpp
@@ -2,10 +2,43 @@
 #include<vector>
 #include<limits.h>
 using namespace std;
+
+int getMax(int brr[][3], int rows, int cols)
+{
+    int max = INT_MIN;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (brr[i][j] > max)
+            {
+                max = brr[i][j];
+            }
+        }
+    }
+    return max;
+}
+
+// minimum value function
+int getMin(int brr[][3], int rows, int cols)
+{
+    int min = INT_MAX;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (brr[i][j] < min)
+            {
+                min = brr[i][j];
+            }
+        }
+    }
+    return min;
+}
 int main(){
     int rows=3;
     int cols=3;
-    int max=INT_MIN;
+    // int max=INT_MIN;
     int arr[3][3];
     int brr[3][3]={{1,2,3},{4,5,6},{6,7,8}};
     // for (int i = 0; i < rows; i++)
@@ -64,24 +97,34 @@ int main(){
     // cout<<endl;
 
     // Find max element in 2d array
-    for (int i = 0; i < rows; i++)
-    {
-        // int sum =0;
-        for (int j = 0; j < cols; j++)
-        {
-            // sum=sum+brr[i][j];
-            // cout<<sum<<" ";
-            if (brr[i][j]>INT_MIN)
-            {
-                max=brr[i][j];
-            }
-            cout<<max<<" ";
-        }
-        // cout<<sum<<" ";
-    }
-    cout<<endl;
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     // int max=INT_MIN;
+    //     // int sum =0;
+    //     for (int j = 0; j < cols; j++)
+    //     {
+    //         // sum=sum+brr[i][j];
+    //         // cout<<sum<<" ";
+    //         if (brr[i][j]>max)
+    //         {
+    //             max=brr[i][j];
+    //             // cout<<max<<" ";
+    //         }
+    //         // cout<<max<<" ";
+    //         // cout<<max<<" ";
+    //     }
+    //     // cout<<sum<<" ";
+    //     cout<<max<<" ";
+    // }
+    // cout<<endl;
 
-    
+    // getMax(brr[3][3],rows,cols);
+    // cout<<getMax<<endl;
+    cout<<"Maximum value :";
+    cout<<getMax(brr,rows,cols)<<endl;
+    cout<<"Minimum value :";
+    cout<<getMin(brr,rows,cols)<<endl;
+
     
 
 
--- arrays/tempCodeRunnerFile.cpp
+++ arrays/tempCodeRunnerFile.cpp
@@ -1,8 +1,2 @@
-for (int i = 0; i < rows; i++)
-    // {
-    //     for (int j = 0; j < cols; j++)
-    //     {
-    //         cin>>arr[i][j];
-    //     }
-    // }
-    // cout<<endl;
+;
+    cout<<getMax<<endl;
