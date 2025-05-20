0d96a2046f76d2f3926cc23d658e77f4ccf7d377
sumit kumar <panditsumit8825@gmail.com>
2025-05-20 10:26:36 +0530

    first changes

--- arrays/chapter3.cpp
+++ arrays/chapter3.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(){
     int rows=3;
     int cols=3;
+    int max=INT_MIN;
     int arr[3][3];
     int brr[3][3]={{1,2,3},{4,5,6},{6,7,8}};
     // for (int i = 0; i < rows; i++)
@@ -15,26 +16,71 @@ int main(){
     //     }
     // }
     // cout<<endl;
-    // cout<<"Printing arrays"<<endl;
-    // for (int i = 0; i < rows; i++)
-    // {
-    //     for (int j = 0; j < cols; j++)
-    //     {
-    //         cout<<brr[i][j]<<" ";
-    //     }
-    //     cout<<endl;
-    // }
-    // cout<<endl;
     cout<<"Printing arrays"<<endl;
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
         {
-            cout<<brr[j][i]<<" ";
+            cout<<brr[i][j]<<" ";
         }
         cout<<endl;
     }
     cout<<endl;
+    // Printing sum row wise
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     int sum =0;
+    //     for (int j = 0; j < cols; j++)
+    //     {
+    //         sum=sum+brr[i][j];
+    //         // cout<<sum<<" ";
+    //     }
+    //     cout<<sum<<" ";
+    // }
+    // cout<<endl;
+    
+    // cout<<"Printing arrays"<<endl;
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     for (int j = 0; j < cols; j++)
+    //     {
+    //         cout<<brr[j][i]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+    // cout<<endl;
+
+    // Printing sum column wise
+    // for (int i = 0; i < rows; i++)
+    // {
+    //     int sum =0;
+    //     for (int j = 0; j < cols; j++)
+    //     {
+    //         sum=sum+brr[j][i];
+    //         // cout<<sum<<" ";
+    //     }
+    //     cout<<sum<<" ";
+    // }
+    // cout<<endl;
+
+    // Find max element in 2d array
+    for (int i = 0; i < rows; i++)
+    {
+        // int sum =0;
+        for (int j = 0; j < cols; j++)
+        {
+            // sum=sum+brr[i][j];
+            // cout<<sum<<" ";
+            if (brr[i][j]>INT_MIN)
+            {
+                max=brr[i][j];
+            }
+            cout<<max<<" ";
+        }
+        // cout<<sum<<" ";
+    }
+    cout<<endl;
+
     
     
 
