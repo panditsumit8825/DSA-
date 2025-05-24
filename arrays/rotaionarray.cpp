e342c88fb49e8aed361e70630003d40dc5ad317d
sumit kumar <panditsumit8825@gmail.com>
2025-05-24 21:46:35 +0530

    first changes

--- arrays/rotaionarray.cpp
+++ arrays/rotaionarray.cpp
@@ -68,40 +68,65 @@
 // }
 
 
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// // comp function to perform sorting
+// bool comp(const string &left, const string &right)
+// {
+//     if (left.size() == right.size())
+//         return left < right;
+
+//     else
+//         return left.size() < right.size();
+// }
+
+
+// vector<string> SortingBigIntegers(vector<string> &arr)
+// {
+//     vector<string> sortArr(arr.begin(), arr.end());
+
+//     // Inbuilt sort function using function as comp
+//     sort(sortArr.begin(), sortArr.end(), comp);
+
+//     return sortArr;
+// }
+
+// // Driver code of above implementation
+// int main()
+// {
+//     vector<string> arr = {"54", "724523015759812365462",
+//                     "870112101220845", "8723"};
+    
+//     vector<string> sortArr = SortingBigIntegers(arr);
+    
+//     for (auto &ele : sortArr)
+//         cout << ele << " ";
+        
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
+
 using namespace std;
 
-// comp function to perform sorting
-bool comp(const string &left, const string &right)
-{
-    if (left.size() == right.size())
-        return left < right;
+int main() {
+  int num = 15;
+  int a = 0, b = 1;
 
-    else
-        return left.size() < right.size();
-}
+  // Here we are printing 0th and 1st terms
+  cout << a << ", " << b << ", ";
 
+  int nextTerm;
 
-vector<string> SortingBigIntegers(vector<string> &arr)
-{
-    vector<string> sortArr(arr.begin(), arr.end());
+  // printing the rest of the terms here
+  for (int i = 2; i < num; i++) {
+    nextTerm = a + b;
+    a = b;
+    b = nextTerm;
 
-    // Inbuilt sort function using function as comp
-    sort(sortArr.begin(), sortArr.end(), comp);
+    cout << nextTerm << ", ";
+  }
 
-    return sortArr;
-}
-
-// Driver code of above implementation
-int main()
-{
-    vector<string> arr = {"54", "724523015759812365462",
-                    "870112101220845", "8723"};
-    
-    vector<string> sortArr = SortingBigIntegers(arr);
-    
-    for (auto &ele : sortArr)
-        cout << ele << " ";
-        
-    return 0;
+  return 0;
 }
