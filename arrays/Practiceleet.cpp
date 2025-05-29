f78dce16972e60548c04c682994b6f8779195f1c
sumit kumar <panditsumit8825@gmail.com>
2025-05-30 00:36:14 +0530

    first changes

--- arrays/Practiceleet.cpp
+++ arrays/Practiceleet.cpp
@@ -1,26 +1,63 @@
+// #include<iostream>
+// #include<vector>
+// #include<limits.h>
+// using namespace std;
+// int main(){
+//     int brr[]={1,2,2,3,3,4,5};
+//     bool isSorted=true;
+//     int n=sizeof(brr)/sizeof(brr[0]);
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (brr[i]>brr[i+1])
+//         {
+//             isSorted = false;
+//             break;
+//         }
+        
+//     }
+//     if (isSorted)
+//     {
+//         cout<<"Sorted array"<<endl;
+//     }
+//     else{
+//         cout<<"Unsorted array"<<endl;
+//     }
+//     return 0;
+// }
+
+
+// find missing element in array
+//  M-1
 #include<iostream>
-#include<vector>
-#include<limits.h>
 using namespace std;
-int main(){
-    int brr[]={1,2,2,3,3,4,5};
-    bool isSorted=true;
-    int n=sizeof(brr)/sizeof(brr[0]);
-    for (int i = 0; i < n; i++)
+void findMissing(int *a , int n){
+    for (int i = 0; i <n; i++)
     {
-        if (brr[i]>brr[i+1])
+        int index = abs(a[i]);
+        if (a[index-1] > 0)
         {
-            isSorted = false;
-            break;
+            a[index - 1] *= -1;
         }
         
     }
-    if (isSorted)
+    // for visiting element
+    for (int i = 0; i < n; i++)
     {
-        cout<<"Sorted array"<<endl;
+        cout<<a[i]<<" ";
     }
-    else{
-        cout<<"Unsorted array"<<endl;
+    cout<<endl;
+    // find missing element
+    for (int i = 0; i < n; i++)
+    {
+        cout<<i+1<<" ";
     }
+    
+    
+}
+int main(){
+int n;
+int a[]={1,3,5,3,4};
+n=sizeof(a)/sizeof(int);
+findMissing(a,n);
     return 0;
 }
