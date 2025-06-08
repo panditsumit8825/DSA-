a7c8ba9746fb6dd0fea370b6c0923a01ac223beb
sumit kumar <panditsumit8825@gmail.com>
2025-06-08 21:17:43 +0530

    first changes

---
+++ arrays/leet_189.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int k;
+    cout<<"Enter the number :";
+    cin>>k;
+    int arr[] ={1,2,3,4,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    num = n-k;
+    int temp[n];
+    for (int i = 0; i<=num; i++)
+    {
+        temp[i] = arr.pop_back(i); 
+
+for (int i = 0; i < k; i++)
+{
+    
+}
+    }
+    
+    return 0;
+}
