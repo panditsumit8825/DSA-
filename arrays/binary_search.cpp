d02ce8dbbce2a0bb5f931ecf519be716771f351e
sumit kumar <panditsumit8825@gmail.com>
2025-06-11 23:48:38 +0530

    irst changes

---
+++ arrays/binary_search.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int target){
+    int start = 0;
+    int end = size - 1;
+
+    int mid = (start + end) / 2;
+    while (start<=end){
+        int element = arr[mid];
+        if(element == target){
+            return mid;
+        }
+        else if(element < target){
+            //  Search in left
+            end = mid + 1;
+        }
+        else{
+            // Search in right
+            start = mid + 1;
+        }
+        mid= (start + end) / 2;
+    }
+    return -1; // Target not found
+}
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = 9;
+    int target = 5;
+    int indexOftarget = binarySearch(arr, size, target);
+    if(indexOftarget == -1){
+        cout<<"Target not found"<<endl;
+    }else{
+        cout<<"Target found at "<<indexOftarget<<endl;
+    }
+    return 0;
+}
